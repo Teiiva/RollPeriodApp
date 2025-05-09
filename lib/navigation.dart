79c91d80a27fa431d7407bcbbc4e09993c98fb3d
dianequeau <diane.queau@isen-ouest.yncrea.fr>
2025-05-09 11:06:29 +0200

    diane

---
+++ lib/navigation.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'widgets/custom_app_bar.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: NavigationPage(),
+    debugShowCheckedModeBanner: false, // Cette ligne enlève la barre de débogage
+  ));
+}
+
+class NavigationPage extends StatelessWidget {
+  const NavigationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(title: "Navigation"),
+      body: Center( // Affiche un texte centré
+        child: Text('Contenu de la page'),
+      ),
+    );
+  }
+}
